01b8cd95abc4582e6395794d143489c249d9226a
Cheeter <juniselmehallawy@gmail.com>
2024-12-19 20:07:43 +0100

    [UPDATE] Mobile Combatibility!

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 ![Other Repos](https://img.shields.io/badge/GitHub-cheeterdev-purple)
 ![License](https://img.shields.io/badge/License-AGPL%203.0-green)
 ![Creator](https://img.shields.io/badge/Creator-0o0kuki0o0-red)
-## This is the Source Code for our Website. Only usable under the AGPL 3.0 License.
+## This is the Source Code for our Website. Only usable under the terms of the AGPL 3.0 License.
--- scripts.js
+++ scripts.js
@@ -5,10 +5,17 @@ function googleTranslateElementInit() {
         layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
         autoDisplay: false
     }, 'google_translate_element');
-    
+
     // Warten und auf Englisch setzen
-    setTimeout(function() {
+    setTimeout(function () {
         translateToLanguage('en');
     }, 500);
 }
+
+// JavaScript to toggle the navigation menu
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    const navbar = document.getElementById('navbar');
+    navbar.classList.toggle('active');
+});
+
 
